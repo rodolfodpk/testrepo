2f6a8f1953105add0f7dfe35cae1040eefa11675
Rodolfo de Paula <rodolfo_dpk@yahoo.com>
2012-06-04 17:15:30 -0300

    thread-safe executor, download as a function

--- build-promoter/src/main/scala/Main.scala
+++ build-promoter/src/main/scala/Main.scala
@@ -13,12 +13,13 @@ import org.apache.commons.codec.digest.DigestUtils._
 import org.apache.commons.codec.digest.DigestUtils
 import java.io.FileOutputStream
 
-case class ArtifactoryServer(val serverUrl: String, val username: String, val password: String) {
+case class ArtifactoryClient(val serverUrl: String, val username: String, val password: String) {
 
+  val http = new Http with thread.Safety // thread-safe executor
+  
   def search(tags: Map[String, String]) = {
 
-    val http = new Http()
-    val querie = url(serverUrl + "/api/search/prop?" + tagsStr(tags)).as_!("admin", "password")
+    val querie = url(serverUrl + "/api/search/prop?" + tagsStr(tags)).as_!(username, password)
     println("GET " + serverUrl + "/api/search/prop?" + tagsStr(tags))
     
     val json = http(querie as_str)
@@ -27,26 +28,28 @@ case class ArtifactoryServer(val serverUrl: String, val username: String, val pa
     val files = parse(json) \ "results"
     val uris = for (f <- files.children) yield f \ "uri"
 
-    http.shutdown()
-    
     implicit val formats = net.liftweb.json.DefaultFormats
     uris map (f => f.extract[String])
 
   }
   
   def donwload(file: String, targetPath: String) = {
-    val querie = url(file).as_!("admin", "password")
-    val http = new Http()
-    println("saving to " + targetPath + "/" + file.split("/").reverse(0))
-    http(querie >>> new FileOutputStream(new File(targetPath + "/" + file.split("/").reverse(0))))
-    http.shutdown()
+    val querie = url(file.replaceFirst("/api/storage", "")).as_!(username, password)
+    println("saving [" + file + "] to " + targetPath + "/" + file.split("/").reverse(0))
+    val targetFile = new File(targetPath + "/" + file.split("/").reverse(0))
+    http(querie >>> new FileOutputStream(targetFile))
+    targetFile
   }
   
   def upload(file: File, repository: String, folder: String, tags: Map[String, String]) {
     if (!file.exists()) throw new IllegalArgumentException("Arquivo não existe!")
     println("PUT " + serverUrl + "/" + repository + ";" + tagsStr(tags) + "/" + targetFolder(file, folder) +  "/" + file.getName() )		
     val destination = url(serverUrl + "/" + repository + ";" + tagsStr(tags) + "/" + targetFolder(file, folder) + "/" + file.getName()).as_!(username, password)
-    Http(destination.PUT  <:< (Map("X-Checksum-MD5" -> md5(file), "X-Checksum-Sha1" -> sha1(file) )) <<< (file, "application/zip") >|)
+    http(destination.PUT  <:< (Map("X-Checksum-MD5" -> md5(file), "X-Checksum-Sha1" -> sha1(file) )) <<< (file, "application/zip") >|)
+  }
+  
+  def shutdown {
+    http.shutdown()
   }
   
   private def sha1(file: File) = {
@@ -77,15 +80,17 @@ object Launcher {
 
   def main(args: Array[String]) {
 
-    val target = new com.example.ArtifactoryServer("http://localhost:8081/artifactory", "admin", "password")
+    val repoClient = new com.example.ArtifactoryClient("http://localhost:8081/artifactory", "admin", "password")
 
     val tags = Map("buildNumber" -> "value1", "revision" -> "value2")
     
-    // val file = new File(...)
-
-    // target.upload(file, "releases-on-qa", "myproject", tags)
+    // val file = new File("/home/rodolfodpk/Downloads/test1/test1-0.0.4.pom")
+    // repoClient.upload(file, "releases-on-qa", "group1", tags)
+    // repoClient.search(tags).par map (x => repoClient.donwload(x, "/home/rodolfodpk/Downloads/test1"))
     
-    target.search(tags).par map (x => target.donwload(x, "/home/rodolfodpk/Downloads"))
+    repoClient.search(tags).par map (x => repoClient.upload(repoClient.donwload(x, "/home/rodolfodpk/Downloads/test1"), "promoted-releases", "group1", tags))
+    
+    repoClient.shutdown
     
   }
 
